50000a554c37f65e99248b134cb77ecf53cbb0f7
prffraia <124781832+prffraia@users.noreply.github.com>
2024-04-15 19:00:57 +0200

    Delete Data Collection/OpenWeather/timestamp.py

--- Data Collection/OpenWeather/timestamp.py
+++
@@ -1,8 +0,0 @@
-import datetime
-# Create a datetime object for February 9th, 2024
-timestamp = 1707515287
-dt_object = datetime.datetime.utcfromtimestamp(timestamp)
-
-# Convert the datetime object to a Unix timestamp
-print("Timestamp:", timestamp)
-print("UTC Date & Time:", dt_object)
